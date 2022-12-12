f7701e784775a69415fb5b7c7ff4d13e938c2fe9
Cadyn Reger <116835253+creger01@users.noreply.github.com>
2022-12-12 16:37:36 -0500

    Add files via upload

---
+++ Q52.py
@@ -0,0 +1,10 @@
+# #create text file
+# write 4 lines
+# loop through fiel twice
+import os
+fout = open("Q52 text.txt")
+
+
+for line in fout:
+    print(line)
+    print(line)
---
+++ Q53.py
@@ -0,0 +1,6 @@
+# make new folder
+# move Q52 to new folder
+
+import os
+os.mkdir("Q3 document")
+os.rename("Q52 text.txt", "Q3 document/Q52 text.txt")
